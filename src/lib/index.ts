e872ca190f62987b26c1ed875c39766a59046aae
Andrej Pavlovic <andrej@ampleinsight.com>
2025-05-30 10:59:32 -0400

    export `WebpageImpactUtils`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export {Co2js} from './co2js/index';
 export {GreenHosting} from './green-hosting/index';
-export {WebpageImpact} from './webpage-impact/index';
+export {WebpageImpact, WebpageImpactUtils} from './webpage-impact/index';
 export {ShellExecCommand} from './shell-exec-command/index';
 export {TimerStart, TimerStop} from './timer/index';
